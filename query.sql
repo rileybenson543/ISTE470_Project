5f8a750b8f273735b6641a69cdad0575d7da2a1d
Riley Basile-Benson (RIT Student) <rdb6619@ist-solace.main.ad.rit.edu>
2022-09-17 14:57:40 -0400

    update query

--- query.sql
+++ query.sql
@@ -1,7 +1,20 @@
-SELECT crew_names.name, title_data.titleName, title_data.averageRating, title_data.numVotes, title_data.genre
+SELECT crew_names.name,  title_data.titleName, title_data.averageRating, title_data.numVotes
 FROM crew_names
 INNER JOIN title_directors ON crew_names.nconst = title_directors.nconst
 INNER JOIN title_data ON title_data.tconst = title_directors.tconst
--- WHERE title_data.numVotes > 1000
+WHERE title_data.numVotes > 1000
 WHERE title_data.titleName IS NOT NULL
+AND title_data.genre LIKE '%comedy%'
+AND title_data.titleType = 'movie'
+-- ORDER BY crew_names.name;
+
+UNION
+
+SELECT crew_names.name,  title_data.titleName, title_data.averageRating, title_data.numVotes
+FROM crew_names
+INNER JOIN title_writers ON crew_names.nconst = title_writers.nconst
+INNER JOIN title_data ON title_data.tconst = title_writers.tconst
+WHERE title_data.numVotes > 1000 
+AND title_data.titleName IS NOT NULL
+AND title_data.genre LIKE '%comedy%'
 -- ORDER BY crew_names.name;
